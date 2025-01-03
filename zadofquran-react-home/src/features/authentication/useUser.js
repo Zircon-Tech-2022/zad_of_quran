7eeba0b56aafbec8f4669f0e3d62d85ea7b42d19
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2025-01-03 09:45:10 +0200

    fix: handled null query user

--- zadofquran-react-home/src/features/authentication/useUser.js
+++ zadofquran-react-home/src/features/authentication/useUser.js
@@ -3,19 +3,20 @@ import { getCurrentUser } from "../../services/apiAuth";
 import { useLangContext } from "../../context/LangContext";
 
 export function useUser() {
-    const { language } = useLangContext();
-    const { isLoading, data: user } = useQuery({
-        queryKey: ["user"],
-        queryFn: () => {
-            if (localStorage.getItem("token")) {
-                return getCurrentUser(localStorage.getItem("token"));
-            }
-        },
-        refetchOnReconnect: false,
-        retry: 0,
-        staleTime: 0,
-        refetchOnWindowFocus: false,
-    });
-    console.log(user);
-    return { isLoading, isAuth: user?.data ? true : false, user };
+  const { language } = useLangContext();
+  const { isLoading, data: user } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        return await getCurrentUser(token); // Ensure this function returns data
+      }
+      return null; // Explicitly return a value when no token exists
+    },
+    refetchOnReconnect: false,
+    retry: 0,
+    staleTime: 0,
+    refetchOnWindowFocus: false,
+  });
+  return { isLoading, isAuth: user?.data, user };
 }
