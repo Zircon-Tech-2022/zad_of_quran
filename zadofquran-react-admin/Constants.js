4fe9cace340ffc8b909965fc218dbfd714e754a2
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2025-01-03 10:11:32 +0200

    dev: updated cofigurations for easier dev env /client.

--- zadofquran-react-admin/Constants.js
+++ zadofquran-react-admin/Constants.js
@@ -1,8 +1,8 @@
-let hostname = window.location.hostname.replace("www.", "");
+const hostname = window.location.hostname.replace("www.", "");
 export const API_URL =
-    hostname === "localhost" || hostname === "127.0.0.1"
-        ? "http://127.0.0.1:3333"
-        : "https://api." + hostname;
+  hostname === "localhost" || hostname === "127.0.0.1"
+    ? import.meta.env.VITE_SERVER_URI
+    : "https://api." + hostname;
 
 // export const API_URL = "https://api.zadofquran-backend.test/";
 export const LIMIT = 15;
